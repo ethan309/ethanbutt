1792c655cb8672714971762bc631dea7eaf8a8a8
Ethan Bütt <ethanbutt309@gmail.com>
2024-04-21 20:16:48 -0500

    Shrink horizontal content padding on smaller screens

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,28 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { pages } from "./pages";
 import NotFound from "./components/NotFound";
 import Home from "./components/Home";
+import { useIsSmallScreen } from "./api/use-is-small-screen";
 
-export const App = () => (
-    <Box textAlign="center" fontSize="xl">
-        <Router>
-            <Grid minH="100vh" py={3} px={9}>
-                <Routes>
-                    {pages.map((page) => (
-                        <Route
-                            key={page.link}
-                            path={page.link}
-                            element={page.element}
-                        />
-                    ))}
-                    <Route path="/" element={<Home />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </Grid>
-        </Router>
-    </Box>
-);
+export const App = () => {
+    const isSmallScreen = useIsSmallScreen();
+
+    return (
+        <Box textAlign="center" fontSize="xl">
+            <Router>
+                <Grid minH="100vh" py={3} px={isSmallScreen ? 3 : 9}>
+                    <Routes>
+                        {pages.map((page) => (
+                            <Route
+                                key={page.link}
+                                path={page.link}
+                                element={page.element}
+                            />
+                        ))}
+                        <Route path="/" element={<Home />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </Grid>
+            </Router>
+        </Box>
+    );
+};
