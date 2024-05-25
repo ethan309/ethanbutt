ce127e4db20c8aba6e4ac53766a32acc8d3cc24b
Ethan Bütt <ethanbutt309@gmail.com>
2024-05-25 00:37:40 -0500

    Make footer more responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,34 @@
-import { Spacer } from "@chakra-ui/react";
+import { HStack, Spacer } from "@chakra-ui/react";
 import React from "react";
+import { useIsSmallScreen } from "../api/use-is-small-screen";
+import ThemeToggle from "./ThemeToggle";
+import ViewSource from "./ViewSource";
+import PrivacyPolicy from "./PrivacyPolicy";
+import SiteMenu from "./Menu";
 
 const Footer = () => {
-    return <Spacer minH="4rem" />;
+    const isSmallScreen = useIsSmallScreen();
+
+    if (isSmallScreen) {
+        return (
+            <HStack minH="4rem">
+                <SiteMenu />
+                <ThemeToggle />
+                <ViewSource />
+                <PrivacyPolicy />
+            </HStack>
+        );
+    }
+
+    return (
+        <>
+            <Spacer minH="4rem" />
+            <SiteMenu isHotCorner />
+            <ThemeToggle isHotCorner />
+            <ViewSource isHotCorner />
+            <PrivacyPolicy isHotCorner />
+        </>
+    );
 };
 
 export default Footer;
