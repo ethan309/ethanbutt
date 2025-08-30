408b8cd92e2da470825a33bfbfac7413e91d3990
Ethan Bütt <ethanbutt309@gmail.com>
2025-08-29 21:36:21 -0500

    Add headline to fun facts

--- src/components/FunFact.tsx
+++ src/components/FunFact.tsx
@@ -1,18 +1,30 @@
-import { Heading, Link, Stack, Text } from "@chakra-ui/react";
-import React from "react";
+import { Heading, HStack, Link, Stack, Text } from "@chakra-ui/react";
+import React, { ReactElement } from "react";
 
 interface IProps {
     heading?: string;
-    fact: string;
+    subheading?: string;
+    fact: string | ReactElement;
     link?: string;
 }
 
 const FunFact = (props: IProps) => {
-    const { heading = "Fun Fact", fact, link } = props;
+    const { heading = "Fun Fact", subheading, fact, link } = props;
 
     return (
         <Stack align="start">
-            <Heading>{heading}</Heading>
+            <HStack
+                w="100%"
+                justifyContent="space-between"
+                alignItems="baseline"
+            >
+                <Heading>{heading}</Heading>
+                {subheading && (
+                    <Text fontSize="sm" as="i">
+                        {subheading}
+                    </Text>
+                )}
+            </HStack>
             <Text textAlign="start">
                 {fact}{" "}
                 <Link href={link} isExternal>
